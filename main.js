08e5886164c9fd8406caeddc898a199ebb5e22da
ActuallyAJL <zero3260@hotmail.com>
2022-02-21 13:06:44 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,32 @@
+const rotateCake = () => {
+	console.log("rotating cake");
+}
+const gatherIngredients = () => {
+	console.log("gathering ingredients");
+}
+const measureIngredients = () => {
+	console.log("measuring ingredients");
+}
+const addFrosting = () => {
+	console.log("adding frosting");
+}
+const startOven = () => {
+	console.log("starting oven");
+}
+const createBatter = () => {
+	console.log("creating batter");
+}
+const checkIfDone = () => {
+	console.log("checking if done");
+}
+const startBaking = () => {
+	console.log("starting to bake");
+}
+startOven()
+gatherIngredients()
+measureIngredients()
+createBatter()
+startBaking()
+rotateCake()
+checkIfDone()
+addFrosting()
